f93c07bdc67e5cc85c726989fdc3c4a50e390ecf
B Kleinen <kleinen@infrastructure.de>
2019-11-26 14:32:48 +0100

    frequency table with array and ArrayList

--- randoms/Die.java
+++ randoms/Die.java
@@ -7,16 +7,44 @@ import java.util.*;
  */
 public class Die
 {
-
     Random random = new Random();
-   
     public int rollDie(){
         return random.nextInt(6) + 1;
     }
+
     public static void demo(){
         Die die = new Die();
         for(int i = 0; i<100; i++){
             System.out.println(die.rollDie());
+        }
+    }
+
+    public static void frequencyTable(){
+        Die die = new Die();
+        int[] frequencyTable = new int[7];
+        for(int i = 0; i<100000; i++){
+            int zahl = die.rollDie();
+            frequencyTable[zahl]++;  
+            //frequencyTable[zahl-1] = frequencyTable[zahl-1] +1;
+        }
+        for(int i = 1; i<=6; i++){
+            System.out.println("Die "+i+" wurde "+
+                frequencyTable[i]+" mal gewürfelt.");         
+        }
+    }
+
+    public static void frequencyTableArrayList(){
+        Die die = new Die();
+        ArrayList<Integer> frequencyTable = new ArrayList<>();
+        for(int i = 0; i<=6; i++) frequencyTable.add(0);
+        for(int i = 0; i<100000; i++){
+            int zahl = die.rollDie();
+            int oldValue = frequencyTable.get(zahl);
+            frequencyTable.set(zahl, oldValue +1);
+        }
+        for(int zahl = 0; zahl<=6; zahl++){
+            System.out.println("Die "+ zahl +" wurde "+
+                frequencyTable.get(zahl) +" mal gewürfelt.");         
         }
     }
 
